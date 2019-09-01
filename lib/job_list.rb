008bbaa564b1ef1e4116ab75eaa815001dd75384
Ravi Prakash <ravi.prakash@techstars.com>
2019-09-02 04:36:27 +0530

    Added logic for circular dependency

--- lib/job_list.rb
+++ lib/job_list.rb
@@ -20,21 +20,47 @@ class JobList
         @final_jobs << Job.new(job_with_dependent[0], job_with_dependent[1])
       end
 
-      jobs_with_dependancy = @final_jobs.select { |job| !job.dependent_id.nil? }
+      circular_dependancies?(@final_jobs.reject { |job| job.dependent_id.nil? })
 
-      jobs_with_dependancy.each do |dependent_job|
-        dependency_index = @final_jobs.find_index { |job| dependent_job.dependent_id == job.value }
-        dependent_index = @final_jobs.find_index { |job| dependent_job.value == job.value }
-        @final_jobs.insert(dependent_index, @final_jobs.delete_at(dependency_index)) if dependency_index > dependent_index
-      end
-
-      @final_jobs
-    rescue SelfDependencyError => e
+      @final_jobs = order_jobs(@final_jobs)
+    rescue SelfDependencyError, CircularDependencyError => e
       puts e.message
+      abort
     end
 
     def jobs
-      return "" if @jobs.length == 0
-      @jobs.collect { |job| job.value }.join("")
+		  @jobs.empty? ? "" : @jobs.collect { |job| job.value }.join("")
+    end
+
+    private
+
+    def order_jobs(jobs)
+      jobs_with_dependancy = jobs.select { |job| !job.dependent_id.nil? }
+
+      jobs_with_dependancy.each do |dependent_job|
+        dependency_index = jobs.find_index { |job| dependent_job.dependent_id == job.value }
+        dependent_index = jobs.find_index { |job| dependent_job.value == job.value }
+        jobs.insert(dependent_index, jobs.delete_at(dependency_index)) if dependency_index > dependent_index
+      end
+      jobs
+    end
+
+    def circular_dependancies?(jobs)
+      job_with_dependancy = Hash.new
+
+      jobs.each do |job|
+        x_id = recursive_find(job_with_dependancy, job.value)
+        y_id = recursive_find(job_with_dependancy, job.dependent_id)
+
+        raise CircularDependencyError, "Circular dependancy detected" if x_id == y_id
+
+        # no circular dependancy so add dependancy to job for next cycle check
+        job_with_dependancy[x_id] = y_id
+      end
+    end
+
+    def recursive_find(job_with_dependancy, id)
+      return id if job_with_dependancy[id].nil? # returns id if no dependancy for that job yet
+      recursive_find(job_with_dependancy, job_with_dependancy[id]) # if there is a dependancy we recursivley find the dependancies depandancy
     end
 end
--- spec/lib/job_list_spec.rb
+++ spec/lib/job_list_spec.rb
@@ -19,15 +19,27 @@ describe "JobList" do
 
   it "should return a jobs sequence in order when jobs are dependent of each other" do
     job_list = JobList.new("a =>\nb => c\nc =>\n")
-		expect(job_list.jobs).to eql "acb"
+		expect(job_list.jobs).to eql("acb")
   end
 
   it "should return a jobs sequence in order when jobs are dependent of more than one jobs" do
     job_list = JobList.new("a =>\nb => c\nc => f\nd => a\ne => b\nf =>\n")
-		expect(job_list.jobs).to eql "afcbde"
+		expect(job_list.jobs).to eql("afcbde")
   end
 
-  it "should return an error when there is a circular dependency on jobs" do
+  context "circular dependency" do
+    it "should return an error when there is a circular dependency on jobs" do
+      expect{JobList.new "a => b\nb => c\nc => a\n"}.to raise_error(SystemExit)
+    end
+
+    it "outputs the exception message to stdout" do
+  				expect{
+  					begin JobList.new "a => b\nb => c\nc => a\n"
+  					rescue SystemExit
+  					end
+  				}.to output("jobs can’t have circular dependencies\n").to_stdout
+		end
   end
+
 
 end
